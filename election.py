141d2403cfce13b430eebc833ae286351e00d539
matthew weaver <matthew@matt-matters.com>
2020-11-02 17:45:38 +0000

    Some refactoring

--- election.py
+++ election.py
@@ -1,22 +1,31 @@
 from voter import voter
+from collections import defaultdict
 
 class election:
 
     def __init__(self, voter_count, options):
-        self.voter_count = voter_count
-        self.options = options
+        self.voter_count = voter_count  
+        self.options = options  # The options to choose from.
         self.rule = None
-        self.voters = []
+        self.voters = []  # Each voter os stored here. 
 
         id = 0 
-        self.vote_counts = {}
+        self.vote_counts = []  # Dictionary stores the total votes for each choice. 
         
+        # Process all voters.
         for v in range(self.voter_count):
-            self.voters.append(voter(id, self.options))
+            # Create a new voter and add to the voters list.
+            new_voter = voter(id, self.options)
+            self.voters.append(new_voter)
 
+
+
+
+
+
+            # The voters choices are in order of preference.
+            # Add up all the first choice votes.
             choice = self.voters[v].options[0]
-            
-            # Does the current vote exist in the dictionary?
             if choice in self.vote_counts:
                 self.vote_counts[choice] += 1   
             else:
@@ -28,10 +37,10 @@ class election:
         self.winning_choice = ''
         self.winning_count = 0
 
-        for key, value in self.vote_counts.items():
-            if value >= highest_vote:
-                highest_vote = value
-                self.winning_choice = key
-                self.winning_count = value
+
+        for choice in sorted(self.vote_counts, key=self.vote_counts.get, reverse=True): 
+            self.winning_choice = 
+            print(choice, self.vote_counts[choice])
+
 
     
